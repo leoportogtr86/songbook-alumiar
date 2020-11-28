6e6c6eb315fa75a4dc07a10f6c8361eee7773676
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-28 10:46:54 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,145 @@
+let pdf = document.querySelector('#pdf')
+let musica1 = document.querySelector('#musica1')
+let musica2 = document.querySelector('#musica2')
+let musica3 = document.querySelector('#musica3')
+let musica4 = document.querySelector('#musica4')
+let musica5 = document.querySelector('#musica5')
+let musica6 = document.querySelector('#musica6')
+let musica7 = document.querySelector('#musica7')
+let musica8 = document.querySelector('#musica8')
+let musica9 = document.querySelector('#musica9')
+let musica10 = document.querySelector('#musica10')
+let musica11 = document.querySelector('#musica11')
+let musica12 = document.querySelector('#musica12')
+let musica13 = document.querySelector('#musica13')
+let musica14 = document.querySelector('#musica14')
+let musica15 = document.querySelector('#musica15')
+let musica16 = document.querySelector('#musica16')
+
+
+musica1.onclick = function () {
+
+    pdf.src = '../assets/pdf/1. Meu sertão.pdf'
+    console.log('musica 1')
+
+}
+
+
+musica2.onclick = function () {
+
+    pdf.src = '../assets/pdf/2. Amanheceu.pdf'
+    console.log('musica 2')
+
+}
+
+
+musica3.onclick = function () {
+
+    pdf.src = '../assets/pdf/3. Alumiar.pdf'
+    console.log('musica 3')
+
+}
+
+
+musica4.onclick = function () {
+
+    pdf.src = '../assets/pdf/4. Eu preciso falar com você.pdf'
+    console.log('musica 4')
+
+}
+
+
+musica5.onclick = function () {
+
+    pdf.src = '../assets/pdf/5. Quer ir mas eu vamos - Full Score.pdf'
+    console.log('musica 5')
+
+}
+
+
+musica6.onclick = function () {
+
+    pdf.src = '../assets/pdf/6. Caba valente.pdf'
+    console.log('musica 6')
+
+}
+
+
+musica7.onclick = function () {
+
+    pdf.src = '../assets/pdf/7. Flor da manhã.pdf'
+    console.log('musica 7')
+
+}
+
+
+musica8.onclick = function () {
+
+    pdf.src = '../assets/pdf/8. Comunhão.pdf'
+    console.log('musica 8')
+
+}
+
+
+musica9.onclick = function () {
+
+    pdf.src = '../assets/pdf/9. Com Muito Louvor.pdf'
+    console.log('musica 9')
+
+}
+
+
+musica10.onclick = function () {
+
+    pdf.src = '../assets/pdf/10. História com H - Partitura completa.pdf'
+    console.log('musica 10')
+
+}
+
+
+musica11.onclick = function () {
+
+    pdf.src = '../assets/pdf/11. Consagração.pdf'
+    console.log('musica 11')
+
+}
+
+
+musica12.onclick = function () {
+
+    pdf.src = '../assets/pdf/12. Vem Com Josué.pdf'
+    console.log('musica 12')
+
+}
+
+
+musica13.onclick = function () {
+
+    pdf.src = '../assets/pdf/13. Galileu.pdf'
+    console.log('musica 13')
+
+}
+
+
+musica14.onclick = function () {
+
+    pdf.src = '../assets/pdf/14. Rebuliço.pdf'
+    console.log('musica 14')
+
+}
+
+
+musica15.onclick = function () {
+
+    pdf.src = '../assets/pdf/15. Feliz.pdf'
+    console.log('musica 15')
+
+}
+
+
+musica16.onclick = function () {
+
+    pdf.src = '../assets/pdf/Eu sou teu - Full Score.pdf'
+    console.log('musica 16')
+
+}
